28bc0e7f66e0a33d3e76460b7e088de3586224ad
bayezale780 <serigne-saliou.fall@etudiant.univ-rennes.fr>
2025-01-10 18:36:35 +0100

    Update StrategieReseau.py

--- StrategieReseau.py
+++ StrategieReseau.py
@@ -2,16 +2,31 @@
 from Terrain import Terrain, Case
 
 class StrategieReseau:
-    def configurer(self, t: Terrain) -> tuple[int, dict[int, tuple[int, int]], list[int]]:
+    def configurer(self, t: Terrain):
         return -1, {}, []
+
+    def valider(self, noeuds, arcs, t: Terrain) -> bool:
+        return True
 
 class StrategieReseauManuelle(StrategieReseau):
-    def configurer(self, t: Terrain) -> tuple[int, dict[int, tuple[int, int]], list[int]]:
-        # TODO
-        return -1, {}, []
+    def configurer(self, t: Terrain):
+        noeud_entree = -1
+        noeuds = {}
+        arcs = []
 
-class StrategieReseauAuto(StrategieReseau):
-    def configurer(self, t: Terrain) -> tuple[int, dict[int, tuple[int, int]], list[int]]:
-        # TODO
-        return -1, {}, []
+        for i, ligne in enumerate(t.cases):
+            for j, c in enumerate(ligne):
+                if c == Case.ENTREE:
+                    noeud_entree = len(noeuds)
+                    noeuds[noeud_entree] = (i, j)
+                elif c == Case.CLIENT:
+                    noeuds[len(noeuds)] = (i, j)
+
+        for n1 in noeuds:
+            for n2 in noeuds:
+                if n1 != n2:
+                    if abs(noeuds[n1][0] - noeuds[n2][0]) + abs(noeuds[n1][1] - noeuds[n2][1]) == 1:
+                        arcs.append((n1, n2))
+
+        return noeud_entree, noeuds, arcs
 
