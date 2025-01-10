9bf97cb14d7118ced77a7257a389a13bf1f7e74e
bayezale780 <serigne-saliou.fall@etudiant.univ-rennes.fr>
2025-01-10 18:38:53 +0100

    Update Reseau.py

--- Reseau.py
+++ Reseau.py
@@ -1,4 +1,3 @@
-
 from Terrain import Terrain, Case
 from StrategieReseau import StrategieReseau, StrategieReseauAuto
 
@@ -7,82 +6,63 @@ class Reseau:
         self.strat = StrategieReseauAuto()
         self.noeuds = {}
         self.arcs = []
-
         self.noeud_entree = -1
 
     def definir_entree(self, n: int) -> None:
-        if n in self.noeuds.keys():
+        if n in self.noeuds:
             self.noeud_entree = n
         else:
             self.noeud_entree = -1
 
-    def ajouter_noeud(self, n: int, coords: tuple[int, int]):
-        if n >= 0:
+    def ajouter_noeud(self, n: int, coords: tuple[int, int]) -> None:
+        if n >= 0 and n not in self.noeuds:
             self.noeuds[n] = coords
 
     def ajouter_arc(self, n1: int, n2: int) -> None:
         if n1 > n2:
-            tmp = n2
-            n2 = n1
-            n1 = tmp
-        if n1 not in self.noeuds.keys() or n2 not in self.noeuds.keys():
-            return
-        if (n1, n2) not in self.arcs:
-            self.arcs.append((n1, n2))
+            n1, n2 = n2, n1  # Assurez-vous que l'ordre est correct
+        if n1 in self.noeuds and n2 in self.noeuds:
+            if (n1, n2) not in self.arcs and (n2, n1) not in self.arcs:
+                self.arcs.append((n1, n2))
 
-    def set_strategie(self, strat: StrategieReseau):
+    def set_strategie(self, strat: StrategieReseau) -> None:
         self.strat = strat
 
     def valider_reseau(self) -> bool:
-        # TODO
-        return False
+        if self.noeud_entree == -1 or self.noeud_entree not in self.noeuds:
+            return False
+
+        visited = set()
+        def dfs(noeud):
+            if noeud in visited:
+                return
+            visited.add(noeud)
+            for arc in self.arcs:
+                if arc[0] == noeud:
+                    dfs(arc[1])
+                elif arc[1] == noeud:
+                    dfs(arc[0])
+
+        dfs(self.noeud_entree)
+        return len(visited) == len(self.noeuds)
 
     def valider_distribution(self, t: Terrain) -> bool:
-        # TODO
-        return False
+        if not isinstance(self.strat, StrategieReseau):
+            raise TypeError("La stratégie doit être une instance de StrategieReseau")
+        return self.strat.valider(self.noeuds, self.arcs, t)
 
     def configurer(self, t: Terrain):
-        self.noeud_entree, self.noeuds, self.arcs  = self.strat.configurer(t)
+        self.noeud_entree, self.noeuds, self.arcs = self.strat.configurer(t)
 
     def afficher(self) -> None:
-        # TODO
-        pass
-
-    def afficher_avec_terrain(self, t: Terrain) -> None:
-        for ligne, l in enumerate(t.cases):
-            for colonne, c in enumerate(l):
-                if (ligne, colonne) not in self.noeuds.values():
-                    if c == Case.OBSTACLE:
-                        print("X", end="")
-                    if c == Case.CLIENT:
-                        print("C", end="")
-                    if c == Case.VIDE:
-                        print("~", end="")
-                    if c == Case.ENTREE:
-                        print("E", end="")
-                    else:
-                        print(" ", end="")
-                else:
-                    if c == Case.OBSTACLE:
-                        print("T", end="")
-                    if c == Case.CLIENT:
-                        print("C", end="")
-                    if c == Case.VIDE:
-                        print("+", end="")
-                    if c == Case.ENTREE:
-                        print("E", end="")
-                    else:
-                        print(" ", end="")
-            print()
+        print("Noeuds:", self.noeuds)
+        print("Arcs:", self.arcs)
 
     def calculer_cout(self, t: Terrain) -> float:
-        cout = 0
-        for _ in self.arcs:
-            cout += 1.5
-        for n in self.noeuds.values():
-            if t[n[0]][n[1]] == Case.OBSTACLE:
+        cout = len(self.arcs) * 1.5
+        for coords in self.noeuds.values():
+            if t.cases[coords[0]][coords[1]] == Case.OBSTACLE:
                 cout += 2
             else:
                 cout += 1
         return cout
-
